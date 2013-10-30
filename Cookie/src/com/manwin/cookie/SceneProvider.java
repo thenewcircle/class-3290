f800abfd2f4fbc86de782c312cc5d42a1016fc99
Marko Gargenta <mgargenta@twitter.com>
2013-10-30 06:38:36 -0700

    Implemented delete() and query()

--- Cookie/src/com/manwin/cookie/SceneProvider.java
+++ Cookie/src/com/manwin/cookie/SceneProvider.java
@@ -7,7 +7,9 @@ import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
 
 public class SceneProvider extends ContentProvider {
@@ -35,7 +37,7 @@ public class SceneProvider extends ContentProvider {
 		case SceneContract.SCENE_DIR:
 			return "vnd.android.cursor.dir/scene";
 		case SceneContract.SCENE_ITEM:
-			return "vnd.android.cursor.item/scene";	
+			return "vnd.android.cursor.item/scene";
 		default:
 			return null;
 		}
@@ -50,7 +52,8 @@ public class SceneProvider extends ContentProvider {
 		db = dbHelper.getWritableDatabase();
 		long rowId = db.insertWithOnConflict(SceneContract.TABLE, null, values,
 				SQLiteDatabase.CONFLICT_REPLACE);
-		if(rowId==-1) throw new SQLiteException("Failed to insert for uri: "+uri);
+		if (rowId == -1)
+			throw new SQLiteException("Failed to insert for uri: " + uri);
 		Uri ret = ContentUris.withAppendedId(uri,
 				values.getAsInteger(SceneContract.Columns.ID));
 		Log.d(TAG, "inserted: " + ret);
@@ -64,16 +67,54 @@ public class SceneProvider extends ContentProvider {
 		return 0;
 	}
 
+	// DELETE from scene WHERE id=47 AND title="%Big%"
 	@Override
 	public int delete(Uri uri, String selection, String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
+		String where;
+		switch (uriMatcher.match(uri)) {
+		case SceneContract.SCENE_DIR:
+			where = selection;
+			break;
+		case SceneContract.SCENE_ITEM:
+			long id = ContentUris.parseId(uri);
+			if (TextUtils.isEmpty(selection)) {
+				where = String.format("%s=%d", SceneContract.Columns.ID, id);
+			} else {
+				where = String.format("%s=%d AND %s", SceneContract.Columns.ID,
+						id, selection);
+			}
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported uri: " + uri);
+		}
+
+		db = dbHelper.getWritableDatabase();
+		return db.delete(SceneContract.TABLE, where, selectionArgs);
 	}
 
+	// SELECT id, title FROM scene WHERE title='%Big%' ORDER BY created_at DESC
 	@Override
 	public Cursor query(Uri uri, String[] projection, String selection,
 			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+		qb.setTables(SceneContract.TABLE);
+
+		switch (uriMatcher.match(uri)) {
+		case SceneContract.SCENE_DIR:
+
+			break;
+		case SceneContract.SCENE_ITEM:
+			long id = ContentUris.parseId(uri);
+			qb.appendWhere(String.format("%s=%d", SceneContract.Columns.ID, id));
+			break;
+		default:
+			throw new IllegalArgumentException("Unsupported uri: " + uri);
+		}
+
+		db = dbHelper.getReadableDatabase();
+		Cursor cursor = qb.query(db, projection, selection, selectionArgs, null, null,
+				sortOrder);
+		Log.d(TAG, "query got records: "+cursor.getCount());
+		return cursor;
 	}
 }
